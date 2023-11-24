31d8d374a2c2da09a3ad83bbb0ec9592de9e1825
Folafolu <ofolafolu@gmail.com>
2023-11-24 22:59:43 +0100

    list tasks

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter import messagebox
 from PIL import Image, ImageTk
-from datetime import datetime
+from datetime import datetime, timedelta
 import sqlite3
-
 
 #DB Settings
 conn = sqlite3.connect('todo.db')
@@ -16,15 +15,64 @@ conn.commit()
 today = datetime.now().strftime("%d/%m/%y")
 i = 0
 
+# List all tasks
+def list_tasks(date):
+    # Clear existing tasks
+    for task in index:
+        task['frm'].destroy()
+        task['delete_btn'].destroy()
+        task['task_lbl'].destroy()
+        task['mark_btn'].destroy()
+    index.clear()
+
+    cursor.execute("SELECT date, task, status FROM employees WHERE date = ?", (date,))
+    rows = cursor.fetchall()
+    print("Found tasks:", rows)
+    print(len(rows))
+    i = 0
+
+    for row in rows:
+        print(date, row[1])
+        task_id = row[0]
+        task_frm = tk.Frame(master=frame, width=375, height=40, background="#444444")
+        task_frm.grid(row=i, column=0, pady=(10, 0))
+
+        task_lbl = ttk.Label(task_frm, text=row[1], font=("Helvetica", 15), background="#444444", foreground="white")
+        task_lbl.place(x=15, y=10)
+
+        delete_btn = tk.Button(task_frm, image=delete_icon, border=0, cursor="hand2", background="#444444", command=lambda: delete_task(delete_btn, task_id))
+        delete_btn.place(x=280, y=10)
+
+        mark_btn = tk.Button(task_frm, image=mark_icon, borderwidth=0, cursor="hand2", background="#444444", border=0, command=lambda: mark_task(mark_btn))
+        mark_btn.place(x=220, y=10)
+
+        task = {'frm': task_frm, 'delete_btn': delete_btn, 'task_lbl': task_lbl, 'mark_btn': mark_btn}
+        index.append(task)
+        i += 1
+
 
 # Task Functions
 # Clear Screen
 def clear_screen():
     task_ent.delete(0, tk.END)
 
+# Change date function
+def change_date(delta):
+    date_format = "%d/%m/%y"
+    current_date = datetime.strptime(date_lbl['text'], date_format)
+    new_date = current_date + timedelta(days=delta)
+    date_lbl['text'] = new_date.strftime(date_format)
+    list_tasks(date_lbl['text'])
+
+def prev_date():
+    change_date(-1)
+
+def next_date():
+    change_date(1)
+
 # Add tasks
 def add_task():
-    global i
+    global i, task_id
     task_text = task_ent.get()
     if task_text == "":
         messagebox.showerror("TODOApp", "Invalid Entry")
@@ -48,7 +96,7 @@ def add_task():
         clear_screen()
         i += 1
 
-        cursor.execute("INSERT INTO employees (date, task, status) VALUES (?, ?, ?)", (today, task_lbl['text'], 0))
+        cursor.execute("INSERT INTO employees (date, task, status) VALUES (?, ?, ?)", (date_lbl["text"], task_lbl['text'], 0))
         conn.commit()
         
         task_id = cursor.lastrowid
@@ -130,6 +178,17 @@ task_ent.place(x=10, y=15, height=39, width=290)
 date_lbl = ttk.Label(master=top_frm, text=today, font=("Georgia", 20, "bold"), foreground="white", background="#333333")
 date_lbl.place(x=120, y=10)
 
+
+# Change date
+# image = Image.open("todoapp\\add.png")
+# add_image_resize = image.resize((30, 34))
+# add_icon = ImageTk.PhotoImage(add_image_resize)
+next_date_btn = tk.Button(master=top_frm, command=next_date,background="black",borderwidth=1, cursor="hand2")
+next_date_btn.place(x=260, y=20)
+
+prev_date_btn = tk.Button(master=top_frm,command=prev_date, background="black",borderwidth=1, cursor="hand2")
+prev_date_btn.place(x=100, y=20)
+
 # Buttons
 # Add task 
 image = Image.open("todoapp\\add.png")
@@ -147,6 +206,8 @@ delete_icon = ImageTk.PhotoImage(delete_image_resize)
 mark_image = Image.open("todoapp\mark.png")
 mark_image_resize = mark_image.resize((25, 25))
 mark_icon = ImageTk.PhotoImage(mark_image_resize)
-
 index = []
+
+list_tasks(today)
+
 window.mainloop()
