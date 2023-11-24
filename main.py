fde1d578f58f9a7c4936d36f6ed7d54641a97784
Folafolu <ofolafolu@gmail.com>
2023-11-24 18:34:01 +0100

    scrollbar

--- main.py
+++ main.py
@@ -9,16 +9,15 @@ import sqlite3
 #DB Settings
 conn = sqlite3.connect('todo.db')
 cursor = conn.cursor()
-
-cursor.execute('''CREATE TABLE IF NOT EXISTS employees
-                (id INTEGER PRIMARY KEY, date TEXT, task TEXT, status REAL)''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS employees(id INTEGER PRIMARY KEY, date TEXT, task TEXT, status REAL)''')
 conn.commit()
 
+# Global Vars
 today = datetime.now().strftime("%d/%m/%y")
 i = 0
-task_id = 0
 
 
+# Task Functions
 # Clear Screen
 def clear_screen():
     task_ent.delete(0, tk.END)
@@ -30,19 +29,17 @@ def add_task():
     if task_text == "":
         messagebox.showerror("TODOApp", "Invalid Entry")
     else:
-        # insert into DB
+        task_frm = tk.Frame(master=frame,  width=375, height=40, background="#444444")
+        task_frm.grid(row=i, column=0,  pady=(10,0))
 
-        task_frm = tk.Frame(master=body_frm,  width=320, height=50, background="#ffffff")
-        task_frm.place(x=10, y=70 + i * 65)
+        task_lbl = ttk.Label(task_frm, text=task_text, font=("Helvetica", 15), background="#444444", foreground="white")
+        task_lbl.place(x=15, y=10)
 
-        task_lbl = ttk.Label(task_frm, text=task_text, font=("Helvetica", 15), background="white")
-        task_lbl.place(x=15, y=15)
+        delete_btn = tk.Button(task_frm, image=delete_icon, border=0, cursor="hand2", background="#444444", command= lambda: delete_task(delete_btn, task_id))
+        delete_btn.place(x=280, y=10)
 
-        delete_btn = tk.Button(task_frm, image=delete_icon, border=0, cursor="hand2", background="white", command= lambda: delete_task(delete_btn, task_id))
-        delete_btn.place(x=280, y=15)
-
-        mark_btn = tk.Button(task_frm, image=mark_icon, borderwidth=0, cursor="hand2", background="white", border=0, command= lambda: mark_task(mark_btn))
-        mark_btn.place(x=220, y=15)
+        mark_btn = tk.Button(task_frm, image=mark_icon, borderwidth=0, cursor="hand2", background="#444444", border=0, command= lambda: mark_task(mark_btn))
+        mark_btn.place(x=220, y=10)
 
         # Keep track of tasks created
         task = {'frm': task_frm,'delete_btn': delete_btn, 'task_lbl': task_lbl, 'mark_btn': mark_btn}
@@ -71,7 +68,7 @@ def delete_task(delete_btn, task_id):
 
             # Update positions of the remaining tasks after the deleted one
             for j, remaining_task in enumerate(index):
-                remaining_task['frm'].place(x=10, y=70 + j * 65)
+                remaining_task['frm'].grid(row=j, column=0,  pady=(20,0))
                 remaining_task['delete_btn'].place(x=280, y=15)
                 remaining_task['task_lbl'].place(x=15, y=15)
                 remaining_task['mark_btn'].place(x=220, y=15)
@@ -83,7 +80,7 @@ def delete_task(delete_btn, task_id):
 
             break  # Break out of the loop once the task is found
 
-# Mark a task as donw with strikethrough
+# Mark a task as done with strikethrough
 def mark_task(mark_btn):
     for task in index:
         if task['mark_btn'] == mark_btn:
@@ -94,40 +91,59 @@ def mark_task(mark_btn):
             cursor.execute("UPDATE employees SET status = ? WHERE task = ?", (1,text))
             conn.commit()
 
-
+# frame configure
+def onFrameConfigure(canvas):
+    '''Reset the scroll region to encompass the inner frame'''
+    canvas.configure(scrollregion=canvas.bbox("all"))
 
 # Tkinter Window
 window = tk.Tk()
-window.geometry("360x450-30+10")
+window.geometry("375x450-30+10")
 window.title("TODO APP")
 window.resizable(False, False)
 
-top_frm = tk.Frame(master=window, width=360, height=50, background="#333333")
-top_frm.pack()
+# ScrollBar
+#------Scrollbars are only activated on Text, Canvas and Listbox widgets-------
+canvas = tk.Canvas(window, borderwidth=0, background="#ffffff")  #create a canva
+frame = tk.Frame(canvas, background="#ffffff") #Put a frame in the canvas
 
-body_frm = tk.Frame(master=window,  width=360, height=400, background="#cccccc")
-body_frm.pack()
+#create sb widget
+vsb = tk.Scrollbar(canvas, orient="vertical", command=canvas.yview)
+canvas.configure(yscrollcommand=vsb.set) #Configure sb on canvas widget
 
+vsb.pack(side="right", fill="y") #postion sb
+canvas.pack(side="left", fill="both", pady=(108,0), expand=True) #position canvas
+canvas.create_window((4,4), window=frame, anchor="nw") 
+frame.bind("<Configure>", lambda event, canvas=canvas: onFrameConfigure(canvas)) 
+
+# All Frames
+top_frm = tk.Frame(master=window, width=375, height=50, background="#333333")
+top_frm.place(x=0, y=0)
+
+ent_frm = tk.Frame(master=window, width=375, height=60, background="white")
+ent_frm.place(x=0, y=50)
+
+task_ent = ttk.Entry(master=ent_frm, font=("Helvetica", 15))
+task_ent.place(x=10, y=15, height=39, width=290)
+
+# Date Label
 date_lbl = ttk.Label(master=top_frm, text=today, font=("Georgia", 20, "bold"), foreground="white", background="#333333")
 date_lbl.place(x=120, y=10)
 
-task_ent = ttk.Entry(master=body_frm, font=("Helvetica", 15))
-task_ent.place(x=10, y=20, height=39, width=290)
-
-
-# Add task Button
+# Buttons
+# Add task 
 image = Image.open("todoapp\\add.png")
 add_image_resize = image.resize((30, 34))
 add_icon = ImageTk.PhotoImage(add_image_resize)
-add_btn = tk.Button(master=body_frm, image=add_icon, command=add_task, borderwidth=1, cursor="hand2")
-add_btn.place(x=315, y=20)
+add_btn = tk.Button(master=ent_frm, image=add_icon, command=add_task, borderwidth=1, cursor="hand2")
+add_btn.place(x=315, y=15)
 
-# Delete Button
+# Delete task
 delete_image = Image.open("todoapp\delete.png")
 delete_image_resize = delete_image.resize((25, 25))
 delete_icon = ImageTk.PhotoImage(delete_image_resize)
 
-# Mark Button
+# # Mark task
 mark_image = Image.open("todoapp\mark.png")
 mark_image_resize = mark_image.resize((25, 25))
 mark_icon = ImageTk.PhotoImage(mark_image_resize)
